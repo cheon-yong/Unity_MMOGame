85cdbadff2f83ca3f0211537ddfe130c557c3e94
전용준 <yes1312@naver.com>
2021-12-14 23:22:39 +0900

    login UI

--- Client/Assets/Scripts/Scenes/GameScene.cs
+++ Client/Assets/Scripts/Scenes/GameScene.cs
@@ -12,10 +12,6 @@ public class GameScene : BaseScene
 
         SceneType = Define.Scene.Game;
 
-        //TODO : 잠시 기생중
-        Managers.Web.BaseUrl = "https://localhost:5001/api";
-        WebPacket.SendCreateAccount("yj", "1234");
-
         Managers.Map.LoadMap(1);
 
         Screen.SetResolution(640, 480, false);
---
+++ Server/SharedDB/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SharedDB
+{
+    public class Class1
+    {
+    }
+}
